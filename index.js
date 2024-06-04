67c361fbda511b6a688ccf34a58d82fbf52f6995
Geovanny Mero <jefferson_mero1911@hotmail.com>
2024-06-03 22:46:43 -0500

    Removed all files modules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello geovanny')
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=> {
+    res.send("<h2>It's working!</h2>")
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+})
