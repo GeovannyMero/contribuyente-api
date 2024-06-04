b328a02b3b2adfc6b3213ca786d5fcc45801636e
Geovanny Mero <jefferson_mero1911@hotmail.com>
2024-06-03 23:01:56 -0500

    versioning

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+//v1
+const v1Router = require('./src/v1/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> {
-    res.send("<h2>It's working!</h2>")
-});
-
+app.use('/api/v1', v1Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
